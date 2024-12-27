17a01d63f21cb40ee63dc7f02f3c6c3aef7c4425
OkinoLeiba <okino.leiba@yahoo.com>
2024-12-26 20:05:37 -0500

    add async functionality and different approach to request user data

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,7 +3,13 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [{"username": "username", "password": "password"}];
+
+let username = "asif";
+let password =  "12345";
+
+const jwtSecret = "244d0b97c61cb978567e348a15fc8cd5c3c5791af982ccae88db48383bc3c273";
+
+let users = [{"username": username, "password": password}];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
@@ -42,6 +48,8 @@ regd_users.post("/login", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
 
+//   const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
 // authenticatedUser(username, password);
   if(username && password) {
     // isValid(username);
@@ -57,10 +65,38 @@ regd_users.post("/login", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
+//only registered users can login
+regd_users.post("/login", (req, res) => {
+    //Write your code here
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const token = jwt.sign({ username: user.username }, jwtSecret, { expiresIn: '1h' });
+        res.status(200).json({ token });
+    } else {
+        res.status(401).json({ message: "Invalid username or password" });
+    }
+});
+
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   const author = req.params.author;
+  const decoded = jwt.verify(token, jwtSecret);
+  const username = decoded.username;
+
+  if (books[isbn]) {
+      if (!books[isbn].reviews) {
+          books[isbn].reviews = {};
+      }
+      books[isbn].reviews[username] = review;
+    
+    res.status(200).json({ message: "Review added/updated successfully" });
+} else {
+    res.status(404).json({ message: "Book not found" });
+}
+
+
   const review = "";
   if(review) {
     Object.values(books).find(a => a.author === author).reviews = review;
@@ -75,9 +111,11 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 // Delete a book review
 regd_users.delete("/auth/review/:isbn", (req, res) => {
     const author = req.params.author;
+    const decoded = jwt.verify(token, jwtSecret);
+    const username = decoded.username;
 
     books = Object.values(books).filter(a => a.title !== isbn);
-
+    delete books[isbn].reviews[username];
     return res.status(300).json({message: "Review deleted!"});
 });
 
